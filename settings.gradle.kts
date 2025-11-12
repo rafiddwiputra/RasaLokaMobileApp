b0eb6531376acfd60dcb60f3c6fb4c16ce45a2f6
RafidPutraa <rizaluchiha77@gmail.com>
2025-11-12 15:07:59 +0700

    Initial project setup: fix Gradle sync & version catalog configuration

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,14 +11,15 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
     }
+
 }
 
 rootProject.name = "RasaLokaApp"
 include(":app")
- 
